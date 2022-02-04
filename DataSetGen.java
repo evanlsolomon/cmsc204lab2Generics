84438da5b116dee55c8342b1aec44dd6ce46b14c
evanlsolomon <evanlsolomon@gmail.com>
2022-02-04 18:33:26 -0500

    Finished lab 2

---
+++ DataSetGen.java
@@ -0,0 +1,48 @@
+public class DataSetGen<T extends Measurable > {
+
+    private double sum;
+    private Measurable maximum;
+    private int count;
+
+
+    public DataSetGen()
+    {
+        sum = 0;
+        count = 0;
+        maximum = null;
+    }
+
+
+    /**
+     Adds a data value to the data set.
+     @param x a data value
+     */
+    public void add(T x)
+    {
+        sum = sum + x.getMeasure();
+        if (count == 0 || maximum.getMeasure() < x.getMeasure())
+            maximum = x;
+        count++;
+    }
+
+
+    /**
+     Gets the average of the added data.
+     @return the average or 0 if no data has been added
+     */
+    public double getAverage()
+    {
+        if (count == 0) return 0;
+        else return sum / count;
+    }
+
+
+    /**
+     Gets the largest of the added data.
+     @return the maximum or 0 if no data has been added
+     */
+    public T getMaximum()
+    {
+        return (T) maximum;
+    }
+}
